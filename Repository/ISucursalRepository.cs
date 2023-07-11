1d8b3542959c4d34f559c44baff6573852714d5a
Elizabeth Alzate <elizabeth.alzatez@perficient.com>
2023-07-11 14:51:37 -0500

    Se actualizan librerías y agrega sqlite

---
+++ Repository/ISucursalRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface ISucursalRepository
+    {
+        List<Sucursal> CreateSucursal(Sucursal sucursal);
+        List<Sucursal> GetAll();
+        Sucursal? GetSucursal(string codigo);
+        Sucursal? UpdateClient(Cliente client, string documento);
+    }
+}
